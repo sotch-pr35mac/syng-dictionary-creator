ac75787d27066a46840435695f5b817e630a3f5c
Preston Wang-Stosur-Bassett <p.wanstobas@gmail.com>
2023-03-27 21:50:06 -0400

    General updates

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,22 @@
 extern crate bincode;
 extern crate hsk;
 extern crate prettify_pinyin;
+extern crate regex;
 extern crate serde;
 extern crate serde_derive;
-extern crate regex;
 
 mod cedict_utils;
 mod dictionary_utils;
 
-use dictionary_utils::{WordEntry, SyngDictionary};
 use cedict_utils as cedict;
+use dictionary_utils::{SyngDictionary, WordEntry};
 
 fn main() {
-	println!("\nBuilding Word List");
-	let word_list: Vec<WordEntry> = cedict::get_cedict_data();
-	println!("\nBuidling Dictionary File");
-	let dictionary: SyngDictionary = dictionary_utils::build_dictionary(word_list);
-	println!("\nWriting Files");
-	dictionary_utils::write_dictionary_files(&dictionary);
-	println!("\nFinished.");
+    println!("\nBuilding Word List");
+    let word_list: Vec<WordEntry> = cedict::get_cedict_data();
+    println!("\nBuidling Dictionary File");
+    let dictionary: SyngDictionary = dictionary_utils::build_dictionary(word_list);
+    println!("\nWriting Files");
+    dictionary_utils::write_dictionary_files(&dictionary);
+    println!("\nFinished.");
 }
